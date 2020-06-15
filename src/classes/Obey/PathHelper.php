5abef3b2092d975894ec05d07f80bec891effa57
Szczepan Hołyszewski <s.holyszewski@zuva.pl>
2020-06-15 09:33:03 +0200

    Some improvements for input and output listing logic

--- src/classes/Obey/PathHelper.php
+++ src/classes/Obey/PathHelper.php
@@ -5,23 +5,30 @@ namespace Obey;
 
 class PathHelper
 {
-    public static function cat(?string ...$segments) : string
+    public static function cat(?string ...$fragments) : string
     {
+        $nonNullFragments = array_filter($fragments,fn($v) => null!==$v);
+        if (0===count($nonNullFragments)) return ".";
+        $explodedFragments = [
+            self::split($nonNullFragments[0]),
+            ...array_map(fn($fragment) => self::split($fragment,false), array_slice($nonNullFragments,1))
+        ];
+        $segments = array_merge(...$explodedFragments);
         if (!count($segments)) return ".";
-        return rtrim(preg_replace('~/+~','/',implode("/",array_map('trim',$segments))),"/");
+        return implode("/",$segments);
     }
     public static function isAbsolute(string $segment) : bool
     {
         //TODO: support windows paths?
         return preg_match('|^/|', $segment);
     }
-    public static function norm(?string ...$segments) : string
+    public static function norm(?string ...$fragments) : string
     {
-        $explodedSegments = array_map(self::class.'::split',$segments);
-        $allSegments = array_merge(...$explodedSegments);
+        $explodedFragments = array_map(self::class.'::split',$fragments);
+        $segments = array_merge(...$explodedFragments);
         $up=0;
         $result=[];
-        foreach($allSegments as $segment) {
+        foreach($segments as $segment) {
             if ($segment===".") continue;
             elseif($segment==="..") {
                 if (count($result)) array_pop($result);
@@ -36,14 +43,16 @@ class PathHelper
 
     /**
      * @param string $path
+     * @param bool $allowAbsolute
      * @return string[]
      */
-    public static function split(?string $path) : array
+    public static function split(?string $path, bool $allowAbsolute = true) : array
     {
-        return
-            "" !== trim(strval($path))
-                ? array_filter(explode("/",$path), fn($v) => ""!==trim(strval($v)))
-                : [];
+        $trimmedPath = trim($path);
+        if (""===$trimmedPath) return [];
+        $wasAbsolute = $allowAbsolute && "/"===$trimmedPath[0] ?? null;
+        $result = array_filter(explode("/",$trimmedPath), fn($v) => ""!==trim($v));
+        return $wasAbsolute ? ["",...$result] : $result;
     }
 
     public static function unprefix(string $path, string $prefix) : string
